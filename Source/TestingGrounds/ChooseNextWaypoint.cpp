edcbed766c8164f86799c937753753d53485dc18
Darren Gresch <darrengresch@gmail.com>
2018-05-09 22:16:46 -0400

    Changed from inheritence to composition. I.e., added a "patrol route" component to our base third person character, and simplified the ChooseNextWaypoint's task of pulling patrol points from each thingymajiger.

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -1,21 +1,26 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright FLB Games.
 
 #include "ChooseNextWaypoint.h"
 #include "GameFramework/Pawn.h"
 #include "AIController.h"
-#include "PatrollingGuard.h" // TODO remove coupling
+#include "PatrolRoute.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8 * NodeMemory)
 {
-	// TODO protect against empty patrol routes
+	// Get the patrol route
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
 
-	// Get the patrol points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
+	//Warn about empty patrol routes
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points"));
+		return EBTNodeResult::Failed;
+	}
 
 	// Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
---
+++ Source/TestingGrounds/PatrolRoute.cpp
@@ -0,0 +1,9 @@
+// Copyright FLB Games.
+
+#include "PatrolRoute.h"
+
+
+TArray<AActor*> UPatrolRoute::GetPatrolPoints() const
+{
+	return PatrolPoints;
+}
